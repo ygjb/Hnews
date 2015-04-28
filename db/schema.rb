7ebaea06cfa1e2cda4c9ae0788fd21cbe318b64d
Rahul Lakhaney <rahul@codebrew.io>
2015-04-28 14:41:00 +0530

    Add Association between link and user

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150428082401) do
+ActiveRecord::Schema.define(version: 20150428090905) do
 
   create_table "links", force: :cascade do |t|
     t.string   "title"
     t.string   "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
